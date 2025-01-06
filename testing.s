6475d537fcb0e4a8bbc27796cd678828f77550d5
CLopMan <cesarlopman@gmail.com>
2025-01-06 17:49:53 +0100

    add.vi and sub.vi instructions

--- testing.s
+++ testing.s
@@ -1,5 +1,9 @@
 .text
 main:
-    li t0 65535
+    li t0 16
     li t1 2
-    vadd.vx v2 v1 t0
+    vadd.vx v1 v1 t0
+    vadd.vx v2 v2 t1
+    vsub.vv v3 v1 v2 
+    vsub.vi v3 v3 1 
+    vadd.vi v3 v3 7
